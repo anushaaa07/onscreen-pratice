5b4003934533158179b31188727f05a6e56e4396
Anusha <anusha.goswami@somaiya.edu>
2022-06-09 01:48:39 +0530

    string methods-1

---
+++ string methods.py
@@ -0,0 +1,16 @@
+a = "coding is nice"
+c_s = a.capitalize()
+print(c_s)
+c_s = a.center(45, '+')
+print(c_s)
+b = "LOLLiPoP"
+l_s = b.casefold()
+print(l_s)
+c = "I love to read books"
+print(c.count('o'))
+print(c.endswith('lol'))
+print(c.encode())
+print(c.find('love'))
+print(c.find('no'))
+d = "Insomnia is bad"
+print(d.index('bad'))
