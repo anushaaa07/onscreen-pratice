ce8ae9bb23ab2800f614beb6fa849a5a31bd6118
Anusha <anusha.goswami@somaiya.edu>
2022-06-09 00:55:24 +0530

    string operations done

---
+++ string operations.py
@@ -0,0 +1,10 @@
+var1="Hie I am Anusha"
+var2="I am 19 years old"
+print(var1 + var2)
+print(var1*3)
+print(var1[9])
+print(var2[0:5])
+print('H' in var1)
+print('H' in var2)
+print('o' in var1)
+print('o' in var2)
