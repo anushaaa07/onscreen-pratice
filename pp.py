bd5320cfd04e8d26e278bc67f553e0e22118e768
Anusha <anusha.goswami@somaiya.edu>
2022-06-09 00:13:46 +0530

    learn basic operations

---
+++ pp.py
@@ -0,0 +1,14 @@
+val1 = 20
+val2 = 10
+res1 = val1 + val2
+res2 = val1 - val2
+res3 = val1 * val2
+res4 = val1 / val2
+res5 = val2 / val1
+res10= val1 % val2
+print(res1)
+print(res2)
+print(res3)
+print(res4)
+print(res5)
+print(res10)
